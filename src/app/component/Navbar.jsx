9b93f913940a9e4148158edfae574f82bbfbd66c
basilkhan1250 <basilkhan1250@gmail.com>
2025-10-05 15:07:35 -0700

    working on Navbar

---
+++ src/app/component/Navbar.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+const Navbar = () => {
+    return (
+        <>
+            <div className="h-[60px] bg-blue-500 p-4 mb-2 text-white">
+                <h1>This is navbar</h1>
+            </div>
+        </>
+    )
+}
+export default Navbar
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
 "use client";
 import Image from "next/image";
 import { Store } from "./component/store";
+import Navbar from "./component/Navbar";
+
 
 export default function Home() {
   // ✅ Call the store as a hook
   const { count, increase, decrease } = Store();
 
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <p>This is counter: {count}</p>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <h1>Welcome to Next.js!</h1>
+        <p>This is counter: {count}</p>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
+      </div>
+    </>
   );
 }
