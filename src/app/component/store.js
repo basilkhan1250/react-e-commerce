0c7c7d58cc7d1db66642924967f1dab239ba1e8a
basilkhan1250 <basilkhan1250@gmail.com>
2025-10-04 22:46:29 -0700

    working on it

---
+++ src/app/component/store.js
@@ -0,0 +1,11 @@
+"use client"
+
+import { create } from "zustand"
+
+export const useStore = create((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    decrease: () => set((state) => ({ count: state.count - 1 })),
+    reset: () => set({ count: 0 }),
+}))
+
